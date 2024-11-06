42898d2d1efb6db432af6397cbc0c8f7f9b6bdcf
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 23:44:32 +0530

    exception pakage created

---
+++ demo/src/main/java/com/example/demo/Exception/DataPersistException.java
@@ -0,0 +1,7 @@
+package com.example.demo.Exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException(){}
+    public DataPersistException(String message){}
+    public DataPersistException(String message ,Throwable cause){}
+}
--- demo/src/main/java/com/example/demo/utill/Mapping.java
+++ demo/src/main/java/com/example/demo/utill/Mapping.java
@@ -5,8 +5,11 @@ import com.example.demo.DTO.IMPL.FieldDTO;
 import com.example.demo.Entity.IMPL.CropEntity;
 import com.example.demo.Entity.IMPL.FieldEntity;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Mapping {
@@ -14,17 +17,23 @@ public class Mapping {
     private ModelMapper modelMapper;
 
     public CropEntity toCropEntity(CropDTO cropDTO) {
+
         return modelMapper.map(cropDTO, CropEntity.class);
     }
-
     public CropDTO toCropDto(CropEntity cropEntity) {
+
         return modelMapper.map(cropEntity, CropDTO.class);
     }
-
+    public List<CropDTO>asCropDtolist(List<CropEntity> cropEntity){
+        return modelMapper.map(cropEntity,new TypeToken<List<CropDTO>>(){}.getType());
+    }
     public FieldEntity toFieldEntity(FieldDTO fieldDTO){
         return modelMapper.map(fieldDTO, FieldEntity.class);
     }
     public FieldDTO toFieldDto(FieldEntity fieldEntity){
         return modelMapper.map(fieldEntity, FieldDTO.class);
+    }
+    public List<FieldDTO> asFieldDtoList(List<FieldEntity>fieldEntities){
+        return modelMapper.map(fieldEntities,new TypeToken<List<FieldDTO>>(){}.getType());
     }
 }

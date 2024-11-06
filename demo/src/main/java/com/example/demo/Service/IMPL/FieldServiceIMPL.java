567920dca5e585a3444d8bd0d61d2da24d4a1c65
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 23:14:53 +0530

    implementing

--- demo/src/main/java/com/example/demo/Service/IMPL/FieldServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/FieldServiceIMPL.java
@@ -5,6 +5,7 @@ import com.example.demo.DTO.FieldStatus;
 import com.example.demo.DTO.IMPL.FieldDTO;
 import com.example.demo.Entity.IMPL.FieldEntity;
 import com.example.demo.Service.FieldService;
+import com.example.demo.utill.AppUtill;
 import com.example.demo.utill.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ public class FieldServiceIMPL implements FieldService {
     private Mapping mapping;
     @Override
     public void saveField(FieldDTO fieldDTO) {
+            fieldDTO.setFieldCode(AppUtill.generateFieldId());
             FieldEntity saveField =fieldDao.save(mapping.toFieldEntity(fieldDTO));
     }
 

a6073d78fbe42f333cd9ae1c94aa7a3a4a68ae25
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 19:52:47 +0530

    added annotations

--- demo/src/main/java/com/example/demo/DAO/FieldDao.java
+++ demo/src/main/java/com/example/demo/DAO/FieldDao.java
@@ -2,6 +2,8 @@ package com.example.demo.DAO;
 
 import com.example.demo.Entity.IMPL.fieldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FieldDao extends JpaRepository<fieldEntity,String> {
 }
--- demo/src/main/java/com/example/demo/DAO/vehicleDao.java
+++ demo/src/main/java/com/example/demo/DAO/vehicleDao.java
@@ -2,6 +2,8 @@ package com.example.demo.DAO;
 
 import com.example.demo.Entity.IMPL.vehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface vehicleDao extends JpaRepository<vehicleEntity,String> {
 }

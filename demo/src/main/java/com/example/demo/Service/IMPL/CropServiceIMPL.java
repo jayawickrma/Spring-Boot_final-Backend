9e271b51d9786fa08d92052c59aecd0b17b9f530
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 22:43:17 +0530

    added UUID random id generator

--- demo/src/main/java/com/example/demo/Service/IMPL/CropServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/CropServiceIMPL.java
@@ -1,18 +1,26 @@
 package com.example.demo.Service.IMPL;
 
+import com.example.demo.DAO.CropDao;
 import com.example.demo.DTO.CropStatus;
 import com.example.demo.DTO.IMPL.CropDTO;
 import com.example.demo.Service.CropService;
+import com.example.demo.utill.AppUtill;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+
 @Service
 @Transactional
 public class CropServiceIMPL implements CropService {
+
+    @Autowired
+    private CropDao cropDao;
     @Override
     public void saveCrop(CropDTO cropDTO) {
+            cropDTO.setCropCode(AppUtill.generateCropId());
 
     }
 
--- demo/src/main/java/com/example/demo/utill/AppUtill.java
+++ demo/src/main/java/com/example/demo/utill/AppUtill.java
@@ -1,8 +1,8 @@
-package com.example.demo.Utill;
+package com.example.demo.utill;
 
 import java.util.UUID;
 
-public class GenerateIds {
+public class AppUtill {
     public static String generateFieldId(){return "Field- "+UUID.randomUUID();}
     public static String generateCropId(){return "Crop -"+UUID.randomUUID();}
 }

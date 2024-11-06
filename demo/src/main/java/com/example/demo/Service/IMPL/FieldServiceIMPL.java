6f387fe3e2311f892f1d04bbf9447f8ff0d12c08
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 23:13:46 +0530

    implementing

--- demo/src/main/java/com/example/demo/Service/IMPL/FieldServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/FieldServiceIMPL.java
@@ -1,8 +1,12 @@
 package com.example.demo.Service.IMPL;
 
+import com.example.demo.DAO.FieldDao;
 import com.example.demo.DTO.FieldStatus;
 import com.example.demo.DTO.IMPL.FieldDTO;
+import com.example.demo.Entity.IMPL.FieldEntity;
 import com.example.demo.Service.FieldService;
+import com.example.demo.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +15,13 @@ import java.util.List;
 @Service
 @Transactional
 public class FieldServiceIMPL implements FieldService {
+    @Autowired
+    private FieldDao fieldDao;
+    @Autowired
+    private Mapping mapping;
     @Override
     public void saveField(FieldDTO fieldDTO) {
-
+            FieldEntity saveField =fieldDao.save(mapping.toFieldEntity(fieldDTO));
     }
 
     @Override
@@ -28,7 +36,7 @@ public class FieldServiceIMPL implements FieldService {
 
     @Override
     public void deleteFields(String fieldCode) {
-
+        fieldDao.deleteById(fieldCode);
     }
 
     @Override
--- demo/src/main/java/com/example/demo/utill/Mapping.java
+++ demo/src/main/java/com/example/demo/utill/Mapping.java
@@ -1,7 +1,9 @@
 package com.example.demo.utill;
 
 import com.example.demo.DTO.IMPL.CropDTO;
+import com.example.demo.DTO.IMPL.FieldDTO;
 import com.example.demo.Entity.IMPL.CropEntity;
+import com.example.demo.Entity.IMPL.FieldEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,5 +19,12 @@ public class Mapping {
 
     public CropDTO toCropDto(CropEntity cropEntity) {
         return modelMapper.map(cropEntity, CropDTO.class);
+    }
+
+    public FieldEntity toFieldEntity(FieldDTO fieldDTO){
+        return modelMapper.map(fieldDTO, FieldEntity.class);
+    }
+    public FieldDTO toFieldDto(FieldEntity fieldEntity){
+        return modelMapper.map(fieldEntity, FieldDTO.class);
     }
 }

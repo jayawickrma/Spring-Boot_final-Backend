b4a85d0b68620cfb61964cb0d2b8ecdd4121e6b8
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 21:18:09 +0530

    creating service interfaces methods

--- demo/src/main/java/com/example/demo/Service/CropService.java
+++ demo/src/main/java/com/example/demo/Service/CropService.java
@@ -1,4 +1,13 @@
 package com.example.demo.Service;
 
+import com.example.demo.DTO.IMPL.cropDTO;
+
+import java.util.List;
+
 public interface CropService {
+    void saveCrop(cropDTO crop);
+    List<cropDTO>getAllCrops();
+    void getCrop(String cropId);
+    void deleteCrop(String cropId);
+    void updateCrop(String cropId,cropDTO crop);
 }
--- demo/src/main/java/com/example/demo/Service/FieldService.java
+++ demo/src/main/java/com/example/demo/Service/FieldService.java
@@ -1,4 +1,14 @@
 package com.example.demo.Service;
 
+import com.example.demo.DTO.IMPL.fieldDTO;
+
+import java.util.List;
+
 public interface FieldService {
+    void saveField(fieldDTO field);
+    List<fieldDTO>getAllFields();
+
+    void deleteFields(String fieldId);
+    void updateField(String fieldId,fieldDTO field);
+
 }

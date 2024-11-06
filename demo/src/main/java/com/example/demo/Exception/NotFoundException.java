ed4729cda11f496b8b4fdc5e16fc3bc4d8f1015b
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 23:46:12 +0530

    NotFoundException class created

---
+++ demo/src/main/java/com/example/demo/Exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.demo.Exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(){}
+    public NotFoundException(String message){}
+    public NotFoundException(String message,Throwable cause){}
+}

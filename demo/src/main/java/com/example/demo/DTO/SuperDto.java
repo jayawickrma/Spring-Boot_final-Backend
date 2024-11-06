6443b8ceebc0db27919590d725d1b59d26bb868a
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 14:30:03 +0530

    created basic interfaces

---
+++ demo/src/main/java/com/example/demo/DTO/SuperDto.java
@@ -0,0 +1,6 @@
+package com.example.demo.DTO;
+
+import java.io.Serializable;
+
+public interface SuperDto extends Serializable {
+}
---
+++ demo/src/main/java/com/example/demo/Entity/SuperEntity.java
@@ -0,0 +1,6 @@
+package com.example.demo.Entity;
+
+import java.io.Serializable;
+
+public interface SuperEntity extends Serializable {
+}

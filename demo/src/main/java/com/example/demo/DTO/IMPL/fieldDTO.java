ac8a3dac43142dcef7d3ee3488ab4684e2334c6b
jayawickrama <nishantharaka2004@gmail.com>
2024-11-06 15:08:09 +0530

    created basic interfaces and there extends and implementations

---
+++ demo/src/main/java/com/example/demo/DTO/IMPL/fieldDTO.java
@@ -0,0 +1,6 @@
+package com.example.demo.DTO.IMPL;
+
+import com.example.demo.DTO.SuperDto;
+
+public class fieldDTO implements SuperDto {
+}
---
+++ demo/src/main/java/com/example/demo/DTO/IMPL/vehicleDTO.java
@@ -0,0 +1,6 @@
+package com.example.demo.DTO.IMPL;
+
+import com.example.demo.DTO.SuperDto;
+
+public class vehicleDTO implements SuperDto {
+}

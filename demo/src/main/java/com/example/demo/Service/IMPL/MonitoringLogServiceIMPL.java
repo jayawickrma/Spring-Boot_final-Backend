feeef23aa40f21fce4ae2eaab1849497dc425a20
jayawickrama <nishantharaka2004@gmail.com>
2024-11-07 23:18:40 +0530

    Monitoring Log Servica class created

--- demo/src/main/java/com/example/demo/Service/IMPL/MonitoringLogServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/MonitoringLogServiceIMPL.java
@@ -1,29 +1,47 @@
 package com.example.demo.Service.IMPL;
 
+import com.example.demo.DAO.MonitoringLogDao;
 import com.example.demo.DTO.IMPL.MonitoringLogDTO;
 import com.example.demo.DTO.MonitoringLogStatus;
+import com.example.demo.Entity.IMPL.MonitoringLogEntity;
+import com.example.demo.Exception.DataPersistException;
 import com.example.demo.Service.MonitoringLogService;
+import com.example.demo.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+@Service
+@Transactional
 
 public class MonitoringLogServiceIMPL implements MonitoringLogService {
+    @Autowired
+    private MonitoringLogDao monitoringLogDao;
+    @Autowired
+    private Mapping mapping;
     @Override
     public void saveLog(MonitoringLogDTO monitoringLogDTO) {
-
+        MonitoringLogEntity saveLog = monitoringLogDao.save(mapping.toMonitoringLogEntity(monitoringLogDTO));
+            if (saveLog==null){
+                throw new DataPersistException("Log Not Found");
+            }
     }
 
     @Override
     public List<MonitoringLogDTO> getAllLogs() {
-        return null;
+        return mapping.asMonitoringDtoList(monitoringLogDao.findAll());
     }
 
     @Override
     public MonitoringLogStatus getLog(String logCode) {
-        return null;
+        MonitoringLogEntity findLog =monitoringLogDao.getReferenceById(logCode);
+        return mapping.toMonitoringLogDto(findLog);
     }
 
     @Override
     public void deleteLog(String logCode) {
+        monitoringLogDao.deleteById(logCode);
 
     }
 

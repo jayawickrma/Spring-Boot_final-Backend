c4af25be636e9e562c655aca29386c8980cd862f
jayawickrama <nishantharaka2004@gmail.com>
2024-11-07 23:22:00 +0530

    staff id generator

--- demo/src/main/java/com/example/demo/utill/IdGenerate.java
+++ demo/src/main/java/com/example/demo/utill/IdGenerate.java
@@ -7,4 +7,5 @@ public class IdGenerate {
     public static String generateCropId(){return "Crop -"+UUID.randomUUID();}
     public static String generateEquipmentID(){return "Equipment -"+UUID.randomUUID();}
     public static String generateLogCode(){return "Log- "+UUID.randomUUID();}
+    public static String generateStaffId(){return "ID- "+UUID.randomUUID();}
 }

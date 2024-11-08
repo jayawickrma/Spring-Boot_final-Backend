2be9a25dde90faf08e6321488640d7031faa8a58
jayawickrama <nishantharaka2004@gmail.com>
2024-11-08 13:58:26 +0530

    error finding

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jakarta.jws.WebParam;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +9,11 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 	@Bean
 	public ModelMapper modelMapper(){return new ModelMapper();}
+
 }

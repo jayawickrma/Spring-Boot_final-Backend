0ddbcf1d1f796c8a0896c39baf07a742af625e63
jayawickrama <nishantharaka2004@gmail.com>
2024-11-14 00:10:37 +0530

    adding security

--- demo/src/main/java/com/example/demo/Service/IMPL/AuthServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/AuthServiceIMPL.java
@@ -24,19 +24,25 @@ public class AuthServiceIMPL implements AuthService {
     @Override
     public JWTAuthResponse signIn(SignIn signIn) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(signIn.getEmail(),signIn.getPassword()));
-         var users=userDao.findByEmail(signIn.getEmail())
+         var user=userDao.findByEmail(signIn.getEmail())
                  .orElseThrow(() -> new RuntimeException("User not found"));
-        var generatedToken=jwtService.generateToken(users);
+        var generatedToken=jwtService.generateToken(user);
         return JWTAuthResponse.builder().token(generatedToken).build();
     }
 
     @Override
     public JWTAuthResponse signUp(UserDTO userDTO) {
-        UserEntity saveUser=mapping.to
+        UserEntity saveUser=userDao.save(mapping.toUserEntity(userDTO));
+        var generatedToken=jwtService.generateToken(saveUser);
+        return JWTAuthResponse.builder().token(generatedToken).build();
     }
 
     @Override
     public JWTAuthResponse refreshToken(String accessToken) {
-        return null;
+       var userName=jwtService.extractUserName(accessToken);
+       var findUser=userDao.findByEmail(userName)
+               .orElseThrow(() -> new RuntimeException("User not found"));
+       var refreshToken=jwtService.refreshToken(findUser);
+       return JWTAuthResponse.builder().token(refreshToken).build();
     }
 }

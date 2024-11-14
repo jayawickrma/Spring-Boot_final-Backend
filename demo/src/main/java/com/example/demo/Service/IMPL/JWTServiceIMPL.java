7d2d31657b8b39fcc52170445d9d6c414cd9923b
jayawickrama <nishantharaka2004@gmail.com>
2024-11-14 20:13:59 +0530

    JWTServiceIMPL editing

--- demo/src/main/java/com/example/demo/Service/IMPL/JWTServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/JWTServiceIMPL.java
@@ -6,11 +6,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import java.security.Key;
 import java.util.Date;

5e504c93d335c742a619c97a799ac37bc4d950c9
jayawickrama <nishantharaka2004@gmail.com>
2024-11-15 22:31:23 +0530

    staff save method creating

--- demo/src/main/java/com/example/demo/DAO/StaffDao.java
+++ demo/src/main/java/com/example/demo/DAO/StaffDao.java
@@ -1,9 +1,13 @@
 package com.example.demo.DAO;
 
+import com.example.demo.Entity.IMPL.CropEntity;
 import com.example.demo.Entity.IMPL.StaffEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffDao extends JpaRepository<StaffEntity,String> {
+    @Query(value = "SELECT * FROM staff WHERE memberCode = (SELECT memberCode FROM staff ORDER BY CAST(SUBSTRING(crop_code, 6) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
+   StaffEntity findLastRowNative();
 }
--- demo/src/main/java/com/example/demo/Service/IMPL/StaffServiceIMPL.java
+++ demo/src/main/java/com/example/demo/Service/IMPL/StaffServiceIMPL.java
@@ -1,8 +1,14 @@
 package com.example.demo.Service.IMPL;
 
+import com.example.demo.DAO.FieldDao;
+import com.example.demo.DAO.MonitoringLogDao;
 import com.example.demo.DAO.StaffDao;
+import com.example.demo.DTO.IMPL.FieldDTO;
+import com.example.demo.DTO.IMPL.MonitoringLogDTO;
 import com.example.demo.DTO.IMPL.StaffDTO;
 import com.example.demo.DTO.StaffStatus;
+import com.example.demo.Entity.IMPL.FieldEntity;
+import com.example.demo.Entity.IMPL.LogEntity;
 import com.example.demo.Entity.IMPL.StaffEntity;
 import com.example.demo.Exception.DataPersistException;
 import com.example.demo.Service.StaffService;
@@ -11,6 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,14 +28,44 @@ public class StaffServiceIMPL implements StaffService {
     private StaffDao staffDao;
     @Autowired
     private Mapping mapping;
+    @Autowired
+    private FieldDao fieldDao;
+    @Autowired
+    private MonitoringLogDao monitoringLogDao;
     @Override
     public void saveStaff(StaffDTO staffDTO) {
+        int number =0;
+        StaffEntity staff =staffDao.findLastRowNative();
+        if (staff!=null){
+            String [] parts=staff.getMemberCode().split("-");
+            number=Integer.parseInt(parts[1]);
+        }
+        staffDTO.setMemberCode("S00"+ ++number);
+        StaffEntity staffEntity=mapping.toStaffEntity(staffDTO);
 
-        StaffEntity saveStaff =staffDao.save(mapping.toStaffEntity(staffDTO));
-            if (saveStaff==null){
-                throw new DataPersistException("Couldn't Find");
+        List<FieldEntity>fieldEntities=new ArrayList<>();
+        for (FieldDTO fieldDTO:staffDTO.getFieldList()){
+            if (fieldDao.existsById(fieldDTO.getFieldCode())){
+                fieldEntities.add(fieldDao.getReferenceById(fieldDTO.getFieldCode()));
             }
-    }
+        }
+        staffEntity.setFieldList(fieldEntities);
+        for (FieldEntity fieldEntity :fieldEntities){
+            fieldEntity.getStaffList().add(staffEntity);
+        }
+
+        List<LogEntity>logEntities =new ArrayList<>();
+        for (MonitoringLogDTO monitoringLogDTO:staffDTO.getLogList()){
+            if (monitoringLogDao.existsById(monitoringLogDTO.getLogCode())){
+                logEntities.add(monitoringLogDao.getReferenceById(monitoringLogDTO.getLogCode()));
+            }
+
+        }
+        staffEntity.setLogList(logEntities);
+        for (LogEntity logEntity:logEntities){
+            logEntity.getStaffList().add(staffEntity);
+
+        }    }
 
     @Override
     public List<StaffDTO> getAllStaff() {

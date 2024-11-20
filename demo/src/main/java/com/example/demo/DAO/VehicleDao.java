538d2aba477ba84f5f9b90b4a8974cb7622b3484
jayawickrama <nishantharaka2004@gmail.com>
2024-11-20 14:42:24 +0530

    vahicle save completed

--- demo/src/main/java/com/example/demo/DAO/VehicleDao.java
+++ demo/src/main/java/com/example/demo/DAO/VehicleDao.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehicleDao extends JpaRepository<VehicleEntity,String> {
-    @Query(value = "SELECT * FROM vehicle WHERE vehicleCode = (SELECT vehicleCode FROM vehicle ORDER BY CAST(SUBSTRING(vehicleCode, 9) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
+    @Query(value = "SELECT * FROM vehicle WHERE vehicle_code = (SELECT vehicle_code FROM vehicle ORDER BY CAST(SUBSTRING(vehicle_code, 9) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
     VehicleEntity findLastRowNative();
 }

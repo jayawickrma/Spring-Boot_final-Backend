67b3dc771ac3d62d9aae359259e8cbb2a5c223a6
jayawickrama <nishantharaka2004@gmail.com>
2024-11-22 23:43:10 +0530

    getAllVehicles created

--- demo/src/main/java/com/example/demo/Controller/VehicleController.java
+++ demo/src/main/java/com/example/demo/Controller/VehicleController.java
@@ -11,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/vehicles")
 public class VehicleController {
@@ -41,6 +43,10 @@ public class VehicleController {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    @GetMapping
+    public List<VehicleDTO>getAll(){
+        return vehicleService.getAllVehicles();
     }
     @PutMapping(value = "/{vehicleCode}",consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void>updateVehicle(@PathVariable("vehicleCode")String vehicleCode,@RequestBody VehicleDTO vehicleDTO){

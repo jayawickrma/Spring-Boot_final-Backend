f68e538ce51bedfad46ed841356e798ffadb2974
jayawickrama <nishantharaka2004@gmail.com>
2024-11-27 22:34:27 +0530

    edited

--- demo/src/main/java/com/example/demo/Controller/EquipmentController.java
+++ demo/src/main/java/com/example/demo/Controller/EquipmentController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/equipments")
+@CrossOrigin
 public class EquipmentController {
     @Autowired
     private EquipmentService equipmentService;

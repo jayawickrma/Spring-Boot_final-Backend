b1e6cfd1e1f63b1180a73cc54d2c79bb0b5a0ae7
jayawickrama <nishantharaka2004@gmail.com>
2024-11-28 19:58:29 +0530

    adding security

---
+++ demo/src/main/java/com/example/demo/DTO/IMPL/PasswordDto.java
@@ -0,0 +1,14 @@
+package com.example.demo.DTO.IMPL;
+
+import jdk.jfr.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordDto {
+    private String email;
+    private String newPassword;
+}

eddff40b393efe2c9e38ce39c2e831940a156bcc
jayawickrama <nishantharaka2004@gmail.com>
2024-11-29 00:03:45 +0530

    security done

--- demo/src/main/java/com/example/demo/Controller/UserController.java
+++ demo/src/main/java/com/example/demo/Controller/UserController.java
@@ -2,6 +2,9 @@ package com.example.demo.Controller;
 
 import com.example.demo.DTO.IMPL.UserDTO;
 import com.example.demo.Exception.DataPersistException;
+import com.example.demo.Security.Responce.JWTAuthResponse;
+import com.example.demo.Security.Secure.SignUp;
+import com.example.demo.Service.AuthenticationService;
 import com.example.demo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,23 +12,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+@RequestMapping("api/v1/auth")
 @CrossOrigin
 @RestController
 public class UserController {
     @Autowired
-    private UserService userService;
+    private AuthenticationService authenticationService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void>saveUser(@RequestBody UserDTO userDTO){
-        try{
-            userService.saveUser(userDTO);
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (
-                DataPersistException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PostMapping("/signUp")
+    public ResponseEntity<JWTAuthResponse> saveUser(@RequestBody SignUp signUp){
+        return ResponseEntity.ok(authenticationService.signUp(signUp));
     }
 }

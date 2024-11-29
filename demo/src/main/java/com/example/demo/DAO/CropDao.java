ce5e994cdc7ff761316b6f1fb96f03f337de6482
jayawickrama <nishantharaka2004@gmail.com>
2024-11-29 14:55:12 +0530

    editing

--- demo/src/main/java/com/example/demo/DAO/CropDao.java
+++ demo/src/main/java/com/example/demo/DAO/CropDao.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CropDao extends JpaRepository<CropEntity,String> {
     @Query(value = "SELECT * FROM crop WHERE crop_code = (SELECT crop_code FROM crop ORDER BY CAST(SUBSTRING(crop_code, 6) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
     CropEntity findLastRowNative();
+    @Query(value = "SELECT * FROM crop ORDER BY CAST(SUBSTRING(crop_code, 6) AS UNSIGNED);",nativeQuery = true)
+    @Override
+    List<CropEntity> findAll();
 }

a6298727de4c40db54598d544fd712cd1334e53a
jayawickrama <nishantharaka2004@gmail.com>
2024-11-29 14:58:05 +0530

    editing

--- demo/src/main/java/com/example/demo/DAO/EquipmentDao.java
+++ demo/src/main/java/com/example/demo/DAO/EquipmentDao.java
@@ -1,10 +1,16 @@
 package com.example.demo.DAO;
 
+import com.example.demo.Entity.IMPL.CropEntity;
 import com.example.demo.Entity.IMPL.EquipmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface EquipmentDao extends JpaRepository<EquipmentEntity,String> {
     @Query(value = "SELECT * FROM equipment WHERE equipment_code = (SELECT equipment_code FROM equipment ORDER BY CAST(SUBSTRING(equipment_code, 11) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
     EquipmentEntity findLastRowNative();
+    @Query(value = "SELECT * FROM equipment ORDER BY CAST(SUBSTRING(equipment_code, 11) AS UNSIGNED);",nativeQuery = true)
+    @Override
+    List<EquipmentEntity> findAll();
 }
